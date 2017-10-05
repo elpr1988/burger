78edf7130063fffde6d4760d140ba17d5d59dd80
Elizabeth Prieto <eprieto.business@gmail.com>
2017-10-04 22:16:05 -0400

    still needs work

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,70 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    var value = ob[key];
+    arr.push(key + "=" + value);
+  }
+
+  return arr.toString();
+}
 
 var orm = {
-	selectAll: function() {
-		var qString = "";
+	selectAll: function(tableInput, cb) {
+		var qString = "SELECT * FROM " + tableInput + ";";
+		connection.query(qString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
 	},
-	insertOne: function() {
+	insertOne: function(table, cols, vals, cb) {
+		qString = "INSERT INTO " + table;
+		
+		qString += " (";
+    qString += cols.toString();
+    qString += ") ";
+    qString += "VALUES (";
+    qString += printQuestionMarks(vals.length);
+    qString += ") ";
 
+    console.log(qString);
+
+    connection.query(qString, vals, function(err, result) {
+    	if (err) {
+    		throw err;
+    	}
+    	cb(result);
+    });
 	},
-	updateOne: function() {
+	updateOne: function(table, objColVals, condition, cb) {
+		var qString = "UPDATE " + table;
 
+		qString += " SET ";
+    qString += objToSql(objColVals);
+    qString += " WHERE ";
+    qString += condition;
+
+    console.log(qString);
+    connection.query(qString, function(err, result) {
+    	if (err) {
+    		throw err;
+    	}
+    	cb(result);
+    });
 	}
-}
+};
+
+module.exports = orm;
