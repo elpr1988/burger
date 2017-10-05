fbb484499c04c01b6ee7e8d5ce8ec4faa625f8e9
Elizabeth Prieto <eprieto.business@gmail.com>
2017-10-05 18:32:25 -0400

    fixing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,22 +9,21 @@ router.get("/", function(req, res) {
 		var hbsObj = {
 			burgers: data
 		};
-		console.log(hbsObj);
 		res.render("index", hbsObj);
 	});
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/burgers", function(req, res) {
 	burger.insertOne([
-		"burger_name", "devoured"
+		"burger_name"
 		], [
-		req.body.burger_name, req.body.devoured
+		req.body.burger_name
 		], function(data) {
 			res.redirect("/");
 		});
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
 	var condition = "id = " + req.params.id;
 	burger.updateOne({
 		devoured: req.body.devoured
